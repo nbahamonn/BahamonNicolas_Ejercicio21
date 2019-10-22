d846266068c076d54715d60bf4fad9daf5e7445b
Default user <n.bahamonn@uniandes.edu.co>
2019-10-22 18:35:49 +0000

    GraficaCoseno

--- Ejercicio21.py
+++ Ejercicio21.py
@@ -2,9 +2,9 @@ import numpy as np
 import matplotlib.pylab as plt
 
 x = np.arange(0, 4*np.pi, 0.01)
-y = np.sin(x)
+y = np.cos(x)
 plt.plot(x,y)
 plt.xlabel('X')
 plt.ylabel('Y')
 plt.show()
-plt.savefig('GraficaSeno.png')
+plt.savefig('GraficaCoseno.png')
